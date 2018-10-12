c03bf1a3531ea67489c8ae99d698ffd31acd9771
fakedouse <vv7allaya@gmail.com>
2018-10-12 20:56:33 +0300

    gridflex

--- app/router.js
+++ app/router.js
@@ -2,39 +2,50 @@ const Router = require('koa-router');
 
 const router = module.exports = new Router;
 
-const nav = [
-    {
-        name: "Home",
-        href: "/"
-    },
-    {
-        name: "Creator",
-        href: "/creator"
-    },
-    {
-        name: "Game",
-        href: "/game"
-    }
-];
+const defopt = {
+    nav: [
+        {
+            name: "Home",
+            href: "/"
+        },
+        {
+            name: "Creator",
+            href: "/creator"
+        },
+        {
+            name: "Game",
+            href: "/game"
+        }
+    ],
+    css: [
+        "blocks.css",
+        "elements.css",
+        "containers.css",
+        "columns.css",
+        "media.css",
+        "blocks.css"
+    ]
+}
 router
     .get('/', async ctx => {
         await ctx.render('index', {
             js: 'script.js',
-            css: ['style.css', 'containers.css'],
-            nav: nav
+            css: defopt.css,
+            nav: defopt.nav
         })
     })
     .get('/creator', async ctx => {
         await ctx.render('pages/svgcreator', {
-            css: 'containers.css',
+            nav: defopt.nav,
+            css: defopt.css,
             js: ['svgsreator.js', 'snap.svg.js']
         })
     })
     .get('/game', async ctx => {
         await ctx.render('index', {
-            css: ['containers.css', 'elements.css'],
             js: ['svgsreator.js', 'snap.svg.js'],
-            nav: nav
+            css: defopt.css,
+            nav: defopt.nav
         })
     })
     .get('*', async ctx => {
